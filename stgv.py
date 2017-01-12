73ca11d32fd39026af3d39811f9df969b4dfc538
eeghor <eeghor@gmail.com>
2017-01-13 01:27:10 +1100

    improved

--- stgv.py
+++ stgv.py
@@ -5,6 +5,7 @@ import pandas as pd
 import pprint
 from collections import namedtuple, defaultdict
 import wikipedia
+import sys
 
 # start from looking up the A-League Wikipedia page
 aleague_wiki_url = wikipedia.page("A-League").url
@@ -13,14 +14,27 @@ aleague_wiki_url = wikipedia.page("A-League").url
 page = requests.get(aleague_wiki_url)
 soup = BeautifulSoup(page.content, 'html.parser')
 
+#SoccerTeam = namedtuple('SoccerTeam', ["full_name", "nicknames", "short_name", "founded", "home_venue", "hv_capacity", "home_league"], verbose=False)
+
+
 team_table = soup.find("th", text=re.compile("Current\s*clubs")).parent.parent
+teamlist = []
 
 for row in team_table.find_all("tr")[2:]:
-	print(row.text)
+	
+	clls = row.find_all("td")
+
+	teamdata = defaultdict(str)  # {"full_name": "Sydney FC", "nickname": "Butterflies",...}
+	teamdata["name"] = clls[0].text.strip()
+	teamdata["city"] = clls[1].text.split(",")[0].strip()
+
+	teamlist.append(teamdata)
+
+print("found {} current teams".format(len(teamlist)))
 
 # A-League team names; these will be used to search on Wikipedia so should be complete enough
-aleague_teams = ["Sydney FC", "Adelaide United", "Brisbane Roar FC", "Melbourne City FC", "Newcastle Jets", "Central Coast Mariners",
-"Melbourne Victory", "Perth Glory", "Wellington Phoenix", "Western Sydney Wanderers"]
+#aleague_teams = ["Sydney FC", "Adelaide United", "Brisbane Roar FC", "Melbourne City FC", "Newcastle Jets", "Central Coast Mariners",
+#"Melbourne Victory", "Perth Glory", "Wellington Phoenix", "Western Sydney Wanderers"]
 
 team_lst = []
 wikis = defaultdict(str)
@@ -29,15 +43,15 @@ wikis = defaultdict(str)
 
 print("retrieving Wikipedia urls...")
 
-for team in aleague_teams:
-	wikis[team] = wikipedia.page(team).url
-	print(wikis[team])
+for team in teamlist:
+	wikis[team["name"]] = wikipedia.page(team["name"]).url
+	print(wikis[team["name"]])
 
-SoccerTeam = namedtuple('SoccerTeam', ["full_name", "nicknames", "short_name", "founded", "home_venue", "hv_capacity", "home_league"], verbose=False)
+#sys.exit("yohaho")
 
 for t in wikis.keys():
 
-	print("grabbing {} info...".format(t), end="")
+	print("getting {} info...".format(t), end="")
 
 	season_line = wikis[t]
 	
@@ -73,7 +87,7 @@ for t in wikis.keys():
 	
 	# get when founded
 	founded_row = short_name_row.next_sibling.next_sibling
-	founded = founded_row.find("td").text.split(";")[0].strip()  # only take wha's before ;
+	founded = founded_row.find("td").text.split(";")[0].strip()  # only take what's before ;
 	
 	# get home venue
 	home_venue_row = founded_row.next_sibling.next_sibling
@@ -87,11 +101,18 @@ for t in wikis.keys():
 	home_league_row = capacity_row.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling
 	home_league = home_league_row.find("a").text
 	
-	
-	team_lst.append(SoccerTeam(full_name=full_name, nicknames=nick_name, 
-						short_name=short_name, founded=founded, 
-						home_venue=home_venue, hv_capacity=venue_capacity, home_league=home_league))
-print(team_lst)
+	for tm in teamlist:
+		if tm["name"] == t:
+			tm["full_name"] = full_name
+			tm["nickname"] = nick_name
+			tm["shortname"] = short_name
+			tm["when_founded"] = founded
+			tm["home_venue"] = home_venue
+			tm["venue_capacity"] = venue_capacity
+			tm["home_league"] = home_league
+
+
+print(teamlist)
 
 
 
